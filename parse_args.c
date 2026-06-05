eb9ccdef904eac2cce97a1f65b0f2849690a9bfd
manuel <manuelcerveralara@gmail.com>
2026-06-05 05:48:43 +0200

    a

--- parse_args.c
+++ parse_args.c
@@ -12,24 +12,45 @@
 
 #include "codexion.h"
 
-static int  is_valid_int(char *str)
+static int	is_valid_int(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9' )
-        return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9' )
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int parse_args(t_master *master, char **argv)
+int	parse_args(t_master *master, char **argv)
 {
-    if (is_valid_int(argv[1]) == 0)
-        return (1);
-    else
-        master->number_of_coders = atoi(argv[1]);
+	if (is_valid_int(argv[1]) == 0)
+		return (1);
+	if (is_valid_int(argv[2]) == 0)
+		return (1);
+	if (is_valid_int(argv[3]) == 0)
+		return (1);
+	if (is_valid_int(argv[4]) == 0)
+		return (1);
+	if (is_valid_int(argv[5]) == 0)
+		return (1);
+	if (is_valid_int(argv[6]) == 0)
+		return (1);
+	if (is_valid_int(argv[7]) == 0)
+		return (1);
+	if (strcmp(argv[8], "fifo") != 0 && strcmp(argv[8], "edf") != 0)
+		return (1);
+	master->number_of_coders = atoi(argv[1]);
+	master->time_to_burnout = (long)atoi(argv[2]);
+	master->time_to_compile = (long)atoi(argv[3]);
+	master->time_to_debug = (long)atoi(argv[4]);
+	master->time_to_refactor = (long)atoi(argv[5]);
+	master->number_of_compiles_required = atoi(argv[6]);
+	master->dongle_cooldown = (long)atoi(argv[7]);
+	master->scheduler = argv[8];
+	return (0);
 }
