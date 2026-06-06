4a8381535624a790ef4040eadee3649b1ddba527
Nueljs <manuelcerveralara@gmail.com>
2026-06-06 18:20:41 +0200

    a

--- codexion.c
+++ codexion.c
@@ -6,7 +6,7 @@
 /*   By: macerver <macerver@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 13:45:02 by macerver          #+#    #+#             */
-/*   Updated: 2026/06/05 16:55:33 by macerver         ###   ########.fr       */
+/*   Updated: 2026/06/06 18:15:59 by macerver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,29 @@
 
 static void	init_master(t_master *master)
 {
+	int	i;
+	
 	master->simulation_running = 1;
 	master->start_time = get_time_ms();
 	pthread_mutex_init(&master->log_mutex, NULL);
 	pthread_mutex_init(&master->sim_mutex, NULL);
 	master->coders = malloc(master->number_of_coders * sizeof(t_coder));
 	master->dongles = malloc(master->number_of_coders * sizeof(t_dongle));
+	i = 0;
+	while (i < master->number_of_coders)
+	{
+		master->coders[i].last_compile_start = 0;
+		master->coders[i].master = &master;
+		master->coders[i].times_compiled = 0;
+		master->coders[i].id = i + 1;
+		master->dongles[i].id = i + 1;
+		master->dongles[i].release_time = 0;
+		master->dongles[i].is_taken = 0;
+		pthread_mutex_init(&master->dongles[i].mutex, NULL);
+		pthread_cond_init(&master->dongles[i].cond, NULL);
+		i++;
+	}
 }
-
 int	main(int argc, char **argv)
 {
 	t_master	master;
